e87ac0b265a367b05ac5eac00b04c8e9238f159f
slovechik <yarikmelnyk39@gmail.com>
2017-04-05 01:37:43 +0300

    add rendering of multiple components

---
+++ src/app/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export class Home extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Blah blah blah new Component!</p>
+            </div>
+        );
+    }
+}
