aa6e08ee55fcf490591d09df451fe970821c8165
slovechik <yarikmelnyk39@gmail.com>
2017-04-06 10:21:54 +0300

    add using of props

--- src/app/index.js
+++ src/app/index.js
@@ -16,7 +16,7 @@ class App extends React.Component {
                 </div>
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Home/>
+                        <Home name={"Slovechik"} age={20}/>
                     </div>
                 </div>
             </div>
